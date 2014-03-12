f76c6b6aedbb9fc2299c315e200b832a21b8280d
Alex <mazdarider23@gmail.com>
2014-03-12 17:08:07 -0500

    Bump up to v1.0.58 to get inline with Jackpal

--- Android.mk
+++ Android.mk
@@ -26,7 +26,8 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := AndroidTerm
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PACKAGE_NAME := Terminal
 
 LOCAL_MODULE_TAGS := optional
 
